31857d712e9d7b074a6e10940ee5e47ad0831773
sac2277 <sachinnlohar@gmail.com>
2024-07-04 14:39:48 +0530

    first commit

---
+++ src/test/java/com/demo/DemoJenkinsB224ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsB224ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
